5d02878ab40f2ce18cb044fb36f6ae38aeab3582
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2026-02-13 21:38:00 +0100

    feat(cpu): add emulator core and registers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+mod gameboy;
+mod registers;
+
+use gameboy::Gameboy;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: cargo run <filename>");
+        return;
+    }
+
+    let file_path = &args[1];
+
+    let mut file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Error opening file: {}", err);
+            return;
+        }
+    };
+
+    let mut buffer = Vec::new();
+    if let Err(err) = file.read_to_end(&mut buffer) {
+        println!("Error reading file: {}", err);
+        return;
+    }
+
+    let mut gameboy = Gameboy::load(buffer.as_slice());
+    gameboy.run();
 }
